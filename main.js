42423cdbade524d84b0e7d85c28931ad2316ef11
saviour kweku dogbey <saviour.k.dogbey92@gmail.com>
2024-07-19 17:29:50 +0100

    Saviour java test siite

---
+++ main.js
@@ -0,0 +1,17 @@
+//variables are
+//var
+//let
+//const
+
+//types of java are
+//int;
+//string;
+//float;
+
+let message = "Hello world!, My name is Saviour";
+
+alert (message);
+
+let firstNunber = 5;
+let secondNumber = 7;
+console.log(firstNunber + secondNumber);
